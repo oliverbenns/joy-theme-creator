31b4e4fcde1ff6041cd9b933e1cc2d665c6c8f94
Oliver Benns <oliverbenns@gmail.com>
2022-10-08 18:38:04 +0100

    Fix list section

--- src/sections/ListSection.tsx
+++ src/sections/ListSection.tsx
@@ -1,28 +1,33 @@
 import * as React from "react";
 import Box from "@mui/joy/Box";
-import options from "./options";
-import List from "@mui/joy/List";
+import options, { Variant } from "./options";
+import List, { ListProps } from "@mui/joy/List";
 import ListItemButton from "@mui/joy/ListItemButton";
+import Typography from "@mui/joy/Typography";
 
 const ListSection = () => {
   return (
     <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
-      {options.colors.map((color) => {
+      {options.variants.map((variant) => {
         return (
-          <List
-            component="nav"
-            sx={{
-              maxWidth: 320,
-              minWidth: 200,
-            }}
-            key={color.id}
-          >
-            <ListItemButton>Lorem</ListItemButton>
-            <ListItemButton color={color.id} variant="soft">
-              Ipsum
-            </ListItemButton>
-            <ListItemButton>Ipsum</ListItemButton>
-          </List>
+          <Box key={variant.label} sx={{ mb: 3 }}>
+            <Typography component="span" level="body1" sx={{ mb: 1 }}>
+              {variant.label}
+            </Typography>
+            <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
+              {options.colors.map((color) => {
+                return (
+                  <List component="nav" key={color.id} sx={{ minWidth: 100 }}>
+                    <ListItemButton>Lorem</ListItemButton>
+                    <ListItemButton color={color.id} variant={variant.id}>
+                      {color.label}
+                    </ListItemButton>
+                    <ListItemButton>Dolor</ListItemButton>
+                  </List>
+                );
+              })}
+            </Box>
+          </Box>
         );
       })}
     </Box>
