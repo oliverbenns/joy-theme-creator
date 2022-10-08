be6b4d839c4a108b8478e979b3d0dcacbe70ef39
Oliver Benns <oliverbenns@gmail.com>
2022-10-08 19:19:18 +0100

    Populate menu section

--- src/sections/MenuSection.tsx
+++ src/sections/MenuSection.tsx
@@ -1,8 +1,37 @@
 import * as React from "react";
 import Box from "@mui/joy/Box";
+import options, { Variant } from "./options";
+import MenuList from "@mui/joy/MenuList";
+import MenuItem from "@mui/joy/MenuItem";
+import Typography from "@mui/joy/Typography";
 
 const MenuSection = () => {
-  return <Box sx={{ display: "flex", gap: 1 }}></Box>;
+  return (
+    <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
+      {options.variants.map((variant) => {
+        return (
+          <Box key={variant.label} sx={{ mb: 3 }}>
+            <Typography component="span" level="body1" sx={{ mb: 1 }}>
+              {variant.label}
+            </Typography>
+            <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
+              {options.colors.map((color) => {
+                return (
+                  <MenuList key={color.id} sx={{ minWidth: 100 }}>
+                    <MenuItem>Lorem</MenuItem>
+                    <MenuItem color={color.id} variant={variant.id}>
+                      {color.label}
+                    </MenuItem>
+                    <MenuItem>Dolor</MenuItem>
+                  </MenuList>
+                );
+              })}
+            </Box>
+          </Box>
+        );
+      })}
+    </Box>
+  );
 };
 
 export default MenuSection;
