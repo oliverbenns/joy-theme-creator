9887b0c5ab2d1e582704de2c60d576e10171e736
Oliver Benns <oliverbenns@gmail.com>
2022-10-08 18:38:10 +0100

    Show all variants in link section

--- src/sections/LinkSection.tsx
+++ src/sections/LinkSection.tsx
@@ -1,19 +1,50 @@
 import * as React from "react";
-import Link from "@mui/joy/Link";
+import Link, { LinkProps } from "@mui/joy/Link";
+import Typography from "@mui/joy/Typography";
 import Box from "@mui/joy/Box";
-import options from "./options";
+import options, { Variant } from "./options";
+
+interface LinkVariant extends Variant {
+  additionalProps?: Partial<LinkProps>;
+}
+
+const linkVariants: LinkVariant[] = [];
+
+options.variants.forEach((variant) => {
+  linkVariants.push(variant, {
+    ...variant,
+    label: `${variant.label} disabled`,
+    additionalProps: { disabled: true },
+  });
+});
 
 const LinkSection = () => {
   return (
-    <Box sx={{ display: "flex", gap: 2 }}>
-      {options.colors.map((color) => {
+    <>
+      {linkVariants.map((variant) => {
         return (
-          <Link key={color.id} color={color.id}>
-            {color.label}
-          </Link>
+          <Box key={variant.id} sx={{ mb: 2 }}>
+            <Typography component="span" level="body1" sx={{ mb: 1 }}>
+              {variant.label}
+            </Typography>
+            <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
+              {options.colors.map((color) => {
+                return (
+                  <Link
+                    key={color.id}
+                    variant={variant.id}
+                    color={color.id}
+                    {...variant.additionalProps}
+                  >
+                    {color.label}
+                  </Link>
+                );
+              })}
+            </Box>
+          </Box>
         );
       })}
-    </Box>
+    </>
   );
 };
 
